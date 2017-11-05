3de92c9afdc2f30dcb2715c4f5affc24f5e51c0d
dzhv <dz.vysniauskas@gmail.com>
2017-11-05 15:21:15 +0200

    Fixing fullpage issue when navigating

--- src/app/components/categories.component.ts
+++ src/app/components/categories.component.ts
@@ -9,18 +9,27 @@ import { Component, OnInit, Input, NgZone } from '@angular/core';
 
 export class CategoriesComponent implements OnInit {
     public title: string;
+    private init: boolean;
 
     constructor(
     ) { 
-        this.title = '';              
+        this.title = ''; 
+        this.init = false;             
     }
     
     ngOnInit(): void {
         document.body.classList.remove('gallery-bg');   // hooray for hacks
         document.body.classList.add('category-bg');
 
-        $(document).ready(function() {
-            (<any>$('#fullpage')).fullpage();
-        });
+        if (!this.init)
+            $(document).ready(function() {
+                (<any>$('#fullpage')).fullpage();
+            });
+
+        this.init = true;
+    }
+
+    ngOnDestroy() {         
+        (<any>$('#fullpage')).fullpage.destroy('all');
     }
 }
