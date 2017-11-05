f808bfc711401058b1f7f71199dcab574eecc03d
dzhv <dz.vysniauskas@gmail.com>
2017-11-05 22:19:25 +0200

    Gallery now has modal dialogs and stuff

--- src/app/components/gallery.component.ts
+++ src/app/components/gallery.component.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/switchMap';
 import { PhotoService } from '../scripts/photoService';
 import { PhotoUploader } from '../scripts/photoUploader';
 import { Photo } from '../scripts/photo';
+import { Cloudinary } from '@cloudinary/angular-4.x';
 
 @Component({
 	selector: 'app-root',
@@ -16,15 +17,18 @@ import { Photo } from '../scripts/photo';
 
 export class GalleryComponent implements OnInit {
     public title: string;
-    public photos: Observable<Photo[]>;    
+    public photos: Observable<Photo[]>;
+    public modalPhotoUrl: string;    
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private photoService: PhotoService,
-        public photoUploader: PhotoUploader
+        public photoUploader: PhotoUploader,
+        private cloudinary: Cloudinary
     ) { 
         this.title = 'Jidrėni-fėni';
+        this.modalPhotoUrl = '';
     }
     
     ngOnInit(): void {
@@ -44,5 +48,14 @@ export class GalleryComponent implements OnInit {
     getPhotos = () => {
         this.photos = this.photoService.getPhotos();
         return this.photos;
-    }     
+    }
+
+    openModal = (photoId: string) => {
+        //this.modalPhotoUrl = this.cloudinary.url(photoId);
+
+        this.modalPhotoUrl = `http://res.cloudinary.com/${this.cloudinary.config().cloud_name}` 
+            + `/image/upload/c_fit,h_600,w_800/${photoId}`;
+
+        (<any>$('#photoModal')).modal();
+    }
 }
